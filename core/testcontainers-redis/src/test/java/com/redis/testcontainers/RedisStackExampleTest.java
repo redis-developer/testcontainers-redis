a14eaccf5886876a1144e5ecefa5455aa7e09276
jruaux <jruaux@gmail.com>
2023-12-04 13:31:49 -0800

    test: Formatted example code

--- core/testcontainers-redis/src/test/java/com/redis/testcontainers/RedisStackExampleTest.java
+++ core/testcontainers-redis/src/test/java/com/redis/testcontainers/RedisStackExampleTest.java
@@ -16,21 +16,22 @@ import com.redis.lettucemod.util.RedisModulesUtils;
 @Testcontainers
 class RedisStackExampleTest {
 
-	@Container
-	private static RedisStackContainer container = new RedisStackContainer(
-			RedisStackContainer.DEFAULT_IMAGE_NAME.withTag(RedisStackContainer.DEFAULT_TAG));
+    @Container
+    private static RedisStackContainer container = new RedisStackContainer(
+            RedisStackContainer.DEFAULT_IMAGE_NAME.withTag(RedisStackContainer.DEFAULT_TAG));
 
-	@Test
-	void testSomethingUsingLettuceMod() {
-		// Retrieve the Redis URI from the container
-		String redisURI = container.getRedisURI();
-		RedisModulesClient client = RedisModulesClient.create(redisURI);
-		try (StatefulRedisModulesConnection<String, String> connection = client.connect()) {
-			RedisModulesCommands<String, String> commands = connection.sync();
-			commands.ftCreate("myIndex", Field.tag("myField").build());
-			IndexInfo indexInfo = RedisModulesUtils.indexInfo(commands.ftInfo("myIndex"));
-			Assertions.assertEquals(1, indexInfo.getFields().size());
-			Assertions.assertEquals("myField", indexInfo.getFields().get(0).getName());
-		}
-	}
+    @Test
+    void testSomethingUsingLettuceMod() {
+        // Retrieve the Redis URI from the container
+        String redisURI = container.getRedisURI();
+        RedisModulesClient client = RedisModulesClient.create(redisURI);
+        try (StatefulRedisModulesConnection<String, String> connection = client.connect()) {
+            RedisModulesCommands<String, String> commands = connection.sync();
+            commands.ftCreate("myIndex", Field.tag("myField").build());
+            IndexInfo indexInfo = RedisModulesUtils.indexInfo(commands.ftInfo("myIndex"));
+            Assertions.assertEquals(1, indexInfo.getFields().size());
+            Assertions.assertEquals("myField", indexInfo.getFields().get(0).getName());
+        }
+    }
+
 }
